ba034e71b3efee0f5e9dfba2ba38df16edebc63b
Luigi Vincent <luigivincenthr@gmail.com>
2015-01-31 15:53:12 -0500

    Update Morse.java

--- Morse.java
+++ Morse.java
@@ -2,33 +2,38 @@
  * Created by Luigi on 12/14/2014. Enum set for MorseCodeConverter
  */
 public enum Morse {
-    MC_1(".----", '1'), MC_2("..---", '2'),MC_3("...--", '3'),
+    MC_1(".----"), MC_2("..---"),MC_3("...--"),
 
-    MC_4("....-", '4'), MC_5(".....", '5'), MC_6("-....", '6'),
+    MC_4("....-"), MC_5("....."), MC_6("-...."),
 
-    MC_7("--...", '7'), MC_8("---..", '8'), MC_9("----.", '9'),
+    MC_7("--..."), MC_8("---.."), MC_9("----."),
 
-    MC_0("-----", '0'), MC_A(".-", 'A'), MC_B("-...", 'B'),
+    MC_0("-----"), MC_A(".-"), MC_B("-..."),
 
-    MC_C("-.-.", 'C'), MC_D("-..", 'D'), MC_E(".", 'E'),
+    MC_C("-.-."), MC_D("-.."), MC_E("."),
 
-    MC_F("..-.", 'F'), MC_G("--.", 'G'), MC_H("....", 'H'),
+    MC_F("..-."), MC_G("--."), MC_H("...."),
 
-    MC_I("..", 'I'), MC_J(".---", 'J'), MC_K("-.-", 'K'),
+    MC_I(".."), MC_J(".---"), MC_K("-.-"),
 
-    MC_L(".-..", 'L'), MC_M("--", 'M'), MC_N("-.", 'N'),
+    MC_L(".-.."), MC_M("--"), MC_N("-."),
 
-    MC_O("---", 'O'), MC_P(".--.", 'P'), MC_Q("--.-", 'Q'),
+    MC_O("---"), MC_P(".--."), MC_Q("--.-"),
 
-    MC_R(".-.", 'R'), MC_S("...", 'S'), MC_T("-", 'T'),
+    MC_R(".-."), MC_S("..."), MC_T("-"),
 
-    MC_U("..-", 'U'), MC_V("...-", 'V'), MC_W(".--", 'W'),
+    MC_U("..-"), MC_V("...-"), MC_W(".--"),
 
-    MC_X("-..-", 'X'), MC_Y("-.--", 'Y'), MC_Z("--..", 'Z'),
+    MC_X("-..-"), MC_Y("-.--"), MC_Z("--.."),
     MC_SPACE("/", ' ');
 
     public final String morse;
     public final char code;
+    
+    Morse(String morse) {
+        this.morse = morse;
+        this.code = name().charAt(name.length() -1);
+    }
 
     Morse(String morse, char code) {
         this.morse = morse;
