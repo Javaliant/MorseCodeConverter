ebbc656725cbc7b39b716c4c21392af9623e0eee
Luigi Vincent <luigivincenthr@gmail.com>
2015-01-31 17:02:22 -0500

    Update MorseCodeConverter.java

--- MorseCodeConverter.java
+++ MorseCodeConverter.java
@@ -1,38 +1,52 @@
+/* Author: Luigi Vincent
+A morse code converter, takes a string and returns the USA morse equivalent
+*/
+
 import java.util.Scanner;
 
-/**
- * Created by Luigi on 12/14/2014.
- */
 public class MorseCodeConverter {
+	static StringBuilder result = new StringBuilder();
+	
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter String to be converted to Morse Code: ");
-        String userInput = input.nextLine();
-        System.out.println(morseConversion(userInput));
+        System.out.println(morseConversion(input.nextLine()));
     }
 
     public static String morseConversion(String s) {
-        String ms = s.toUpperCase().trim(); // modified string
-        StringBuilder result = new StringBuilder();
+    	/* Uniforms letter case,
+    	gets rid of leading, trailing and consective spaces */
+    	String ms = s.toUpperCase().trim().replaceAll("\\s+", " ");
+
+    	StringBuilder invalidMorse = new StringBuilder();
 
         for (int i = 0; i < ms.length(); i++) {
-            isValid(ms.charAt(i));
-
-            for (Morse c : Morse.values()) {
-                if (ms.charAt(i) == ' ' && ms.charAt(i - 1) == ' '); // Ignore consecutive spaces
-                else if (ms.charAt(i) == c.code) {
-                    result.append(c.morse);
-                    if (i != ms.length() - 1) { result.append((" ")); }
-                }
+            if (!isValidMorse(ms.charAt(i))) {
+            	invalidMorse
+            		.append(ms.charAt(i))
+            		.append(", ")
+            	;
             }
         }
-        return result.toString();
+
+        if (invalidMorse.length() > 0) {
+        	invalidMorse.setLength(invalidMorse.length() - 2);
+	        invalidMorse.append('.');
+	        System.out.println("Ignored input: " + invalidMorse.toString());
+        }
+        return result.toString().trim();
     }
 
-    public static boolean isValid(char c) {
+    public static boolean isValidMorse(char c) {
         for (Morse m : Morse.values()) {
-            if (c == m.code) return true;
+            if (c == m.code) {
+            	result
+                    .append(m.morse)
+                    .append(' ')
+                ;
+                return true;
+            }
         }
-        throw new IllegalArgumentException("Invalid Entry: " + c + ". Non-space input may only be: 0-9, A-Z, or a-z");
+        return false;
     }
 }
